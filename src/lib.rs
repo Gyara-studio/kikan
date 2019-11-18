53b467268fc72d0b6fb675d0355f4d131f610b5d
gyara <me@gyara.moe>
2019-11-18 19:02:55 +0900

    rename hooks_set to hooks

--- src/lib.rs
+++ src/lib.rs
@@ -9,31 +9,31 @@ pub type Status = std::collections::HashMap<String, String>;
 
 pub struct Runtime {
     stat: Status,
-    hooks_set: std::collections::HashMap<String, Hook>,
+    hooks: std::collections::HashMap<String, Hook>,
 }
 
 impl Runtime {
     pub fn new() -> Runtime {
         Runtime {
             stat: Status::new(),
-            hooks_set: std::collections::HashMap::new(),
+            hooks: std::collections::HashMap::new(),
         }
     }
 
     pub fn regist_hook(&mut self, hook: Hook) {
         let name = hook.get_name();
-        self.hooks_set.insert(name, hook);
+        self.hooks.insert(name, hook);
     }
 
     pub fn trigger_hook(&mut self, hook_name: &String) {
-        if let Some(hook) = self.hooks_set.get(hook_name) {
+        if let Some(hook) = self.hooks.get(hook_name) {
             hook.trigger(&mut self.stat);
         }
     }
 
     pub fn new_block(&mut self, hook_name: String, title: String, code: String, enable: bool) {
         let block = Block::new(code, title, enable);
-        if let Some(hook) = self.hooks_set.get_mut(&hook_name) {
+        if let Some(hook) = self.hooks.get_mut(&hook_name) {
             hook.insert_block(block);
         } else {
             let mut hook = Hook::new(hook_name);
