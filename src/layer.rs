9e392fcc4d1231a0d9e60734a57590c8afa5ea0e
gyara <me@gyara.moe>
2021-08-12 11:57:00 +0900

    いちからやる

--- src/layer.rs
+++
@@ -1,7 +0,0 @@
-pub(crate) trait Layer {
-    type Output;
-    type Input;
-    type Err;
-
-    fn update(&mut self, input: Self::Input) -> Result<Self::Output, Self::Err>;
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,1 @@
-mod command;
-mod layer;
-mod parse;
 
-pub fn set_up() -> anyhow::Result<()> {
-    Ok(())
-}
-
-pub fn update(_input: String) -> anyhow::Result<()> {
-    Ok(())
-}
